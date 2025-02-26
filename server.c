bd6ae1e874cf91b0b30f5f0e7b042982b6be90bd
“Faris <fjiwad@gwu.edu>
2025-02-26 10:33:08 -0500

    Made changes to handle event loop properly

--- server.c
+++ server.c
@@ -13,8 +13,7 @@
 #include <signal.h>
 #include <stdbool.h>
 
-void
-unlink_domain_socket(int status, void *filename)
+void unlink_domain_socket(int status, void *filename)
 {
     unlink(filename);
     free(filename);
@@ -23,15 +22,13 @@ unlink_domain_socket(int status, void *filename)
 #define MAX_BUF_SZ 128
 #define MAX_FDS 16
 
-
-void sigint_handler(int sig){
+void sigint_handler(int sig)
+{
     // Perform cleanup or other actions before exiting
     exit(0); // Exit the program
-
 }
 
-void
-server(int num_clients, char *filename)
+void server(int num_clients, char *filename)
 {
     char buf[MAX_BUF_SZ];
     struct pollfd poll_fds[MAX_FDS];
@@ -41,104 +38,101 @@ server(int num_clients, char *filename)
     int num_fds = 0;
 
     /* should do proper cleanup */
-    if (socket_desc < 0){
-        exit(EXIT_FAILURE); 
-    } 
+    if (socket_desc < 0)
+    {
+        exit(EXIT_FAILURE);
+    }
     on_exit(unlink_domain_socket, strdup(filename));
 
-    //init the poll fds
+    // init the poll fds
     memset(poll_fds, 0, sizeof(struct pollfd) * MAX_FDS);
 
-    //the service is itself is a file descriptor, so add to the table of fds
-    poll_fds[num_fds++] = (struct pollfd) {
-        .fd     = socket_desc,
+    // the service is itself is a file descriptor, so add to the table of fds
+    poll_fds[num_fds++] = (struct pollfd){
+        .fd = socket_desc,
         .events = POLLIN,
     };
 
     printf("SQL Server Starting!\n");
-    for (;;) {
+    for (;;)
+    {
 
         printf("Polling...\n");
         int ret = poll(poll_fds, num_fds, -1);
-        if (ret == -1) exit(EXIT_FAILURE);
-
+        if (ret == -1)
+            exit(EXIT_FAILURE);
 
         printf("Polled something\n");
-    
-        
-        //accept a new client
-        if (poll_fds[0].revents & POLLIN) {
 
-            if ((new_client = accept(socket_desc, NULL, NULL)) == -1){
+        // accept a new client
+        if (poll_fds[0].revents & POLLIN)
+        {
+
+            if ((new_client = accept(socket_desc, NULL, NULL)) == -1)
+            {
                 exit(EXIT_FAILURE);
             }
 
-            //add it to the polling list
-            poll_fds[num_fds++] = (struct pollfd) {
+            // add it to the polling list
+            poll_fds[num_fds++] = (struct pollfd){
                 .fd = new_client,
-                .events = POLLIN
-            };
+                .events = POLLIN};
             poll_fds[0].revents = 0;
 
-            //successful connection
+            // successful connection
             printf("Server: New client connected with new file descriptor %d.\n", new_client);
             fflush(stdout);
         }
 
-    
+        // Event loop that handles when new clients are connected
+        for(i = 1; i < num_fds; i++){
+            // Open file to send back to client
+            int fd = open("query_results.txt", O_RDONLY);
+            if (fd == -1)
+            {
+                perror("open");
+                exit(EXIT_FAILURE);
+            }
 
-        //read the message
-        /*
-        amnt = read(new_client, buf, MAX_BUF_SZ - 1);
-        if (amnt == -1) exit(EXIT_FAILURE);
-        buf[amnt] = '\0'; 
-        printf("Server received message (sz %d): \"%s\". Replying!\n", amnt, buf);
-        fflush(stdout);
-        */
+            // send the client a reply 
+            send_fd(new_client, fd);
+            
+            // Now we close the client connection descriptor
+            printf("server: closing client connection %d\n", poll_fds[i].fd);
+            close(poll_fds[i].fd);
+            poll_fds[i] = poll_fds[num_fds - 1];
+            num_fds--;
+            i--;
 
-        // Open file to send
-        int fd = open("query_results.txt", O_RDONLY);
-        if (fd == -1) {
-            perror("open");
-            exit(EXIT_FAILURE);
         }
-
-        /* send the client a reply */
-        // if (write(new_client, buf, amnt) < 0) exit(EXIT_FAILURE);
-        send_fd(new_client, fd);
-        printf("Received file descriptor: %d\n", fd);
-        /* Done with communication with this client */
-        close(new_client);
     }
     close(socket_desc);
-
     exit(EXIT_SUCCESS);
 }
 
-
-
-int
-main(void)
+int main(void)
 {
     char *channel_name = "db_server";
-    int nclients = 2;
+    int nclients = 4;
     int i;
 
     // Signal Cleanup Handling
-    if (signal(SIGINT, sigint_handler) == SIG_ERR) {
+    if (signal(SIGINT, sigint_handler) == SIG_ERR)
+    {
         perror("signal");
         return 1;
     }
 
-    //start the
-    if (fork() == 0){
+    // start the
+    if (fork() == 0)
+    {
         server(nclients, channel_name);
     }
     /* wait for the server to create the domain socket */
 
-
     /* wait for all of the children */
-    while (wait(NULL) != -1);
+    while (wait(NULL) != -1)
+        ;
 
     return 0;
 }

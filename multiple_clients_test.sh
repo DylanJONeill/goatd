0900706b5b2d286733b000cdc3dfa20105c0a9ba
derekgwu <dchen36@gwu.edu>
2025-03-04 10:43:08 -0500

    comments

--- multiple_clients_test.sh
+++ multiple_clients_test.sh
@@ -14,17 +14,20 @@ sleep 2
 #run multiple clients
 ./multiple_clients > /dev/null & 
 
-
+#let the clients setup and connect
 sleep 5
 
+#check the output of the server
 output=$(cat lvl2_test.txt | grep READER)
 
+#if the server asserted that a reader connected, we are good
 if [[ -z "$output" ]]; then
     echo "Test Failed: Server did not poll/accept reader clients"
 else
     echo "Test Passed: Server polled/accepted reader clients"
 fi
 
+#if the server assert that a reader connected we are good
 output=$(cat lvl2_test.txt | grep WRITER)
 
 if [[ -z "$output" ]]; then

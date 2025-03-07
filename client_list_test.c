aa2de8d7e5e8c43eca556ade657ee0a2b778c22a
DylanJONeill <oneilldylanj@gmail.com>
2025-03-06 19:28:28 -0500

    improved test

--- client_list_test.c
+++ client_list_test.c
@@ -20,26 +20,35 @@ void sigint_handler(int sig) //From server.c
     exit(0);
 }
 
-void make_writer(){
-    pid_t writer = fork();
-    if(writer == 0){
-        char* args[] = {"./client", "writer", NULL};
-        execvp(args[0], args);
-        exit(0);
-    }
-}
-
 int main() {
     char *channel_name = "db_server";
     //Make server instance before running test
-  
-    for(int i = 0; i < MAX_CLIENTS; i++){
-        make_writer(); //Makes MAX_CLIENTS writers, should fill up client list
+    pid_t total_children[MAX_CLIENTS] = {-1};
+    for(int i = 0; i < MAX_CLIENTS; ++i){ //Makes MAX_CLIENTS writers, should fill up client list
+        pid_t writer = fork();
+        if(writer == 0){
+            char* args[] = {"./client", "writer", NULL};
+            execvp(args[0], args);
+            exit(0);
+        } else if (writer < 0) {
+        printf("RE RUN TEST\n");
+        exit(EXIT_FAILURE);
+        } 
+        total_children[i] = getpid();
         //printf("Writer %d connected\n", i);
+    }
+
+    sleep(3); //Wait for all children to be initialised
+
+    for (int j = 0; j < MAX_CLIENTS; ++j) {
+        if (total_children[j] == -1) {
+            printf("CLIENT LIST TEST FAILED\n");
+            return -1;
+        }
     }
 
     //If all successfully connect, test passed - 8 clients can work at once
 
-    printf("CLIENT LIST TEST PASSED\n");
+    printf("\nCLIENT LIST TEST PASSED\n");
     return 0;
 }
